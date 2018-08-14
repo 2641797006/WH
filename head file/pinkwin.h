b060602959c4ff3ff15ba5853c1ed4226b1aed06
PinkHat <2641797006@qq.com>
2018-08-14 16:06:58 +0800

    WhiteHat 6.66

---
+++ head file/pinkwin.h
@@ -0,0 +1,227 @@
+/*
+ * <windows.h> should be include before this file
+ * pinkwin.h
+ * char s[1024+]; should be add in main.c file 
+ *
+ */
+
+#ifndef _PINKWIN_H_
+#define _PINKWIN_H_
+//-mwin32 || -mwindows
+
+#ifndef _PINKRES_H_
+#include <__pink/pinkres.h>
+//windres picon.rc picon.o
+#endif
+
+#ifdef _WINDOWS_H
+
+#ifndef _STRING_H
+ __attribute__((__cdecl__)) __attribute__((__nothrow__)) char *strcat (char *, const char *);
+#endif
+
+#define pWinMain(pint1,pint2,pc,vint) int __attribute__((__stdcall__)) WinMain (HINSTANCE pint1, HINSTANCE pint2, LPSTR pc, int vint)
+#define pinkwin() pWinMain(hInstance,hPrevInstance,lpCmdLine,nCmdShow)
+
+#define pWinClass(name,value) const char name[]=value
+
+#define pWinFunc(WndProc,pint,uint1,uint2,vlong) LRESULT (__attribute__((__stdcall__)) WndProc) (HWND pint, UINT uint1, WPARAM uint2, LPARAM vlong)
+#define pinkfunc() pWinFunc(WndProc,hwnd,Message,wParam,lParam)
+#define pWinCFun(FuncType,WndProc,pint,uint1,uint2,vlong) FuncType (__attribute__((__stdcall__)) WndProc) (HWND pint, UINT uint1, WPARAM uint2, LPARAM vlong)
+#define pinkcfun(FuncType,FuncName) pWinCFun(FuncType,FuncName,hwnd,Message,wParam,lParam)
+
+#define pinkwc(wc,ClassName,Picon,pColorRGB)\
+			WNDCLASSEX wc;\
+			wc.cbSize	=sizeof(WNDCLASSEX);\
+			wc.style	=CS_HREDRAW | CS_VREDRAW;\
+			wc.lpfnWndProc	=WndProc;\
+			wc.cbClsExtra	=0;\
+			wc.cbWndExtra	=0;\
+			wc.hInstance	=hInstance;\
+			wc.hIcon	=LoadIcon (hInstance, MAKEINTRESOURCE(Picon)) ;\
+			wc.hCursor	=LoadCursor(NULL, IDC_ARROW);\
+			wc.hbrBackground=CreateSolidBrush(pColorRGB);\
+			wc.lpszMenuName	=MAKEINTRESOURCE(PM_STDM);\
+			wc.lpszClassName=ClassName;\
+			wc.hIconSm	=LoadIcon(hInstance, MAKEINTRESOURCE(Picon));
+
+#define pRegisterClass(wc)\
+		if(!RegisterClassEx(&wc)){\
+			MessageBox(NULL, "Window Registration Failed!", "Error!",MB_ICONEXCLAMATION | MB_OK);\
+			return 0;\
+		}
+
+#define pCreateWindow(hwnd,ClassName,WindowName)\
+		hwnd=CreateWindowEx(\
+			WS_EX_CLIENTEDGE,\
+			ClassName,\
+			WindowName,\
+			WS_OVERLAPPEDWINDOW,\
+			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,\
+			NULL, NULL, hInstance, NULL);\
+		if(hwnd == NULL){\
+			MessageBox(NULL, "Window Creation Failed!", "Error!",MB_ICONEXCLAMATION | MB_OK);\
+			return 0;\
+		}
+
+#define pGetMessage(Msg)\
+		while(GetMessage(&Msg, NULL, 0, 0) > 0){\
+			TranslateMessage(&Msg);\
+			DispatchMessage(&Msg);\
+		}
+
+#define pDrawTextEx0(hwnd,text){\
+		HDC dt0_hdc;\
+		PAINTSTRUCT dt0_ps;\
+		RECT dt0_rect;\
+		dt0_hdc=BeginPaint(hwnd,&dt0_ps);\
+		GetClientRect(hwnd,&dt0_rect);\
+		DrawTextEx(dt0_hdc,text,-1,&dt0_rect,DT_SINGLELINE|DT_CENTER|DT_VCENTER,NULL);\
+		EndPaint(hwnd,&dt0_ps);\
+	}
+
+#define pExeName(FileName) GetModuleFileName(GetModuleHandle(NULL), FileName, MAX_PATH);
+
+#define pinkwinA() pWinClass(_P_win_class_,"_P_pink_value_");\
+		pinkwin(){\
+			pinkwc(wc,_P_win_class_,PI_WH,0x86ffb3);\
+			pRegisterClass(wc);\
+			HWND hwnd;\
+			char exePathName[MAX_PATH+32], P_flag_str[32]=" - WhiteHat 6.66";\
+			pExeName(exePathName);\
+			strcat(exePathName,P_flag_str);\
+			pCreateWindow(hwnd,_P_win_class_,exePathName);\
+			ShowWindow(hwnd,nCmdShow);\
+			UpdateWindow(hwnd);\
+			MSG Msg;\
+			pGetMessage(Msg);\
+			return Msg.wParam;\
+		}
+
+#define pGetWindowWH(hwnd,width,height){\
+		RECT rect;\
+		GetWindowRect(hwnd,&rect);\
+		width=rect.right-rect.left, height=rect.bottom-rect.top;\
+	}
+
+#define pGetLastError(){\
+		char pErrorCode[16];\
+		sprintf(pErrorCode,"%d",GetLastError());\
+		MessageBox(hwnd,pErrorCode,"P Error Code",MB_OK);\
+	}
+
+void pReportErrorFunc(hwnd,pErrorCode,ErrorCodeID)
+{
+	pErrorCode+=ErrorCodeID<<16;
+	char pReportStr[128];
+	sprintf(pReportStr, "Error Code: 0x%08X\nSend the Error-Code to the Author?", pErrorCode);
+	int x = MessageBox(hwnd, pReportStr, "Error!  --ReportError()", MB_OKCANCEL | MB_ICONWARNING);
+	if(x==IDOK){
+		sprintf(pReportStr, "mailto:yongxxone@gmail.com?subject=Report Error Code(WH6.66)&body=Error Code: 0x%08X%%0d%%0a%%0d%%0a%%0d%%0a%%0d%%0a", pErrorCode);
+		ShellExecute(hwnd, "open", pReportStr, NULL, NULL, SW_SHOWNORMAL);
+	}
+}
+
+#define pReportError(hwnd,ErrorCodeID){\
+		int pErrorCode=GetLastError();\
+		if(pErrorCode!=0)\
+			pReportErrorFunc(hwnd,pErrorCode,ErrorCodeID);\
+	}
+
+#define pCoutInt(value){\
+		char s[32];\
+		sprintf(s,"value=%d, hex=0x%X",(int)(value), (int)(value));\
+		MessageBox(hwnd,s,"pCoutInt(value)",MB_OK);\
+	}
+
+#define pCoutDouble(value){\
+		sprintf(s,"value=%Lf",(double)(value));\
+		MessageBox(hwnd,s,"pCoutDouble(value)",MB_OK);\
+	}
+
+#define pCoutStr(string)		MessageBox(NULL,string,"pCoutStr(string)",MB_OK)
+
+#define pCoutFont(hdc){\
+		char s[32];\
+		GetTextFace(hdc,32,s);\
+		MessageBox(hwnd,s,"pCoutFont(hdc)",MB_OK);\
+	}
+
+#define pSetWinFont(hwnd,defFont)	SendMessage(hwnd, WM_SETFONT, (WPARAM)GetStockObject(defFont), (LPARAM)TRUE)
+
+#define pSetCwinFont(hwnd,id,defFont)	SendMessage(GetDlgItem(hwnd,id), WM_SETFONT, (WPARAM)defFont, (LPARAM)TRUE)
+
+#define pDesktopHwnd(hwnd){\
+		hwnd=FindWindow("Progman","Program Manager");\
+		hwnd=FindWindowEx(hwnd, NULL, "SHELLDLL_DefView", NULL);\
+		hwnd=FindWindowEx(hwnd, NULL, "SysListView32", "FolderView");\
+	}
+
+#define pKey_Win_D(){\
+		keybd_event(VK_LWIN, 0, 0, 0);\
+		keybd_event('D', 0, 0, 0);\
+		keybd_event('D', 0, KEYEVENTF_KEYUP, 0);\
+		keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);\
+	}
+
+#define p2Key(key1,key2){\
+		keybd_event(key1, 0, 0, 0);\
+		keybd_event(key2, 0, 0, 0);\
+		keybd_event(key2, 0, KEYEVENTF_KEYUP, 0);\
+		keybd_event(key1, 0, KEYEVENTF_KEYUP, 0);\
+	}
+
+#define pSetToTop(hwnd)			SetWindowPos(hwnd,HWND_TOPMOST,0,0,0,0,SWP_NOMOVE | SWP_NOSIZE)
+
+#define pSetNoTop(hwnd)			SetWindowPos(hwnd,HWND_NOTOPMOST,0,0,0,0,SWP_NOMOVE | SWP_NOSIZE)
+
+#define pGetAdmin(){\
+		pExeName(s);\
+		ShellExecute(hwnd, "runas", s, NULL, NULL, SW_SHOWNORMAL);\
+		pReportError(hwnd,0xad00);\
+		DestroyWindow(hwnd);\
+	}
+
+#define pGetMenu2(hwnd, x)		GetSubMenu(GetMenu(hwnd), x)
+
+#define pGetMenu3(hwnd, x, y)		GetSubMenu(pGetMenu2(hwnd,x), y)
+
+#define pSetChecked(hwnd, x, y)		CheckMenuItem(pGetMenu2(hwnd, x), y, MF_BYPOSITION | MF_CHECKED)
+
+#define pSetUnchecked(hwnd, x, y)	CheckMenuItem(pGetMenu2(hwnd, x), y, MF_BYPOSITION | MF_UNCHECKED)
+
+#define pSetRadio3(hwnd, x, y, s, e, z)	CheckMenuRadioItem(pGetMenu3(hwnd, x, y), s, e, z, MF_BYPOSITION)
+
+#define pSetChecked3(hwnd, x, y, z)	CheckMenuItem(pGetMenu3(hwnd, x, y), z, MF_BYPOSITION | MF_CHECKED)
+
+#define pSetUnchecked3(hwnd, x, y, z)	CheckMenuItem(pGetMenu3(hwnd, x, y), z, MF_BYPOSITION | MF_UNCHECKED)
+
+#define pGetScreenWH(x, y){\
+		x=GetSystemMetrics(SM_CXSCREEN);\
+		y=GetSystemMetrics(SM_CYSCREEN);\
+	}
+
+#define pTaskbarHwnd()			FindWindow("Shell_traywnd", NULL)
+
+#define pRoundDouble(doubleV)			( (long long)(doubleV+0.5) )
+
+#define pDiv(x, y)			pRoundDouble( ((double)x) / ((double)y) )
+
+#define pHeightCM(longv){\
+		HDC hdc;\
+		hdc=GetDC(NULL);\
+		longv=pDiv( GetDeviceCaps(hdc,LOGPIXELSY) , 2.54 );\
+		ReleaseDC(NULL, hdc);\
+	}
+
+#define pWidthCM(longv){\
+		HDC hdc;\
+		hdc=GetDC(NULL);\
+		longv=pDiv( GetDeviceCaps(hdc,LOGPIXELSX) , 2.54 );\
+		ReleaseDC(NULL, hdc);\
+	}
+
+#define pCreateFont(height, fontname)	CreateFont(height, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, fontname)
+
+#endif
+#endif
